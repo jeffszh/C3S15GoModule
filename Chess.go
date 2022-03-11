1b8dc8885f635b30a38a5de7aaafc75ac704fa31
Jeff <zhsu@amwares.com>
2022-03-11 17:58:41 +0800

    棋盘和棋子。

---
+++ Chess.go
@@ -0,0 +1,60 @@
+package model
+
+import "image/color"
+
+type ChessType int
+
+const (
+	ChessTypeEmpty ChessType = iota
+	ChessTypeCannon
+	ChessTypeSoldier
+)
+
+type chess struct {
+	chessType ChessType
+}
+
+func (c *chess) Type() ChessType {
+	return c.chessType
+}
+
+func (c *chess) SetType(chessType ChessType) {
+	c.chessType = chessType
+}
+
+func (c *chess) Text() string {
+	switch c.Type() {
+	case ChessTypeEmpty:
+		return " "
+	case ChessTypeCannon:
+		return "炮"
+	case ChessTypeSoldier:
+		return "兵"
+	default:
+		return ""
+	}
+}
+
+func (c *chess) Color() color.Color {
+	switch c.Type() {
+	case ChessTypeEmpty:
+		return color.Black
+	case ChessTypeCannon:
+		return color.RGBA{R: 255, A: 255}
+	case ChessTypeSoldier:
+		return color.RGBA{B: 255, A: 255}
+	default:
+		return color.Transparent
+	}
+}
+
+type Chess interface {
+	Type() ChessType
+	SetType(chessType ChessType)
+	Text() string
+	Color() color.Color
+}
+
+func NewChess(chessType ChessType) Chess {
+	return &chess{chessType: chessType}
+}
---
+++ Scene.go
@@ -0,0 +1,5 @@
+package model
+
+type Scene struct {
+	chessList [25]*Chess
+}
