a452e07fbf53d25dc3f2476faf6ee774326d8ce2
Jeff <zhsu@amwares.com>
2022-03-15 11:11:59 +0800

    实现移动合法性的判断。

--- Move.go
+++ Move.go
@@ -14,25 +14,73 @@ func (m *moveStruct) To() int {
 }
 
 func (m *moveStruct) FromXY() (x int, y int) {
-	return IndexToXY(m.from)
+	return indexToXY(m.from)
 }
 
 func (m *moveStruct) ToXY() (x int, y int) {
-	return IndexToXY(m.to)
+	return indexToXY(m.to)
 }
 
+// IsValid 判断Move是否合法
 func (m *moveStruct) IsValid(scene Scene) bool {
 	fromX, fromY := m.FromXY()
 	toX, toY := m.ToXY()
+	dx := toX - fromX
+	dy := toY - fromY
+	if dx < 0 {
+		dx = -dx
+	}
+	if dy < 0 {
+		dy = -dy
+	}
+
+	fromType := scene.ChessList()[m.From()].Type()
+	toType := scene.ChessList()[m.To()].Type()
+	movingSide := scene.MovingSide()
+
+	// 首先不能超出棋盘的范围
 	for _, val := range []int{fromX, fromY, toX, toY} {
 		if val < 0 || val >= 6 {
 			return false
 		}
 	}
-	if scene.MovingSide() != scene.ChessList()[m.From()].Type() {
+	// 起点必须是当前回合该走棋一方的棋子
+	if movingSide != fromType {
 		return false
 	}
-	// TODO: 移动到的位置的合法性判断
+
+	// 移动到的终点，若是兵方，
+	if movingSide == ChessTypeSoldier {
+		// 必须是空格，
+		if toType != ChessTypeEmpty {
+			return false
+		}
+		// 且必须是移动1格。
+		if dx+dy != 1 {
+			return false
+		}
+	} else if movingSide == ChessTypeCannon {
+		// 炮方有两种情况
+		if toType == ChessTypeEmpty {
+			// 若是移动，必须是移动1格。
+			if dx+dy == 1 {
+				return true
+			}
+		}
+		// 吃的情况
+		if toType == ChessTypeSoldier {
+			// 必须是移动两步
+			if dx+dy == 2 && dx*dy == 0 {
+				// 且中间是空格
+				midX := (fromX + toX) / 2
+				midY := (fromY + toY) / 2
+				if scene.ChessList()[xyToIndex(midX, midY)].Type() == ChessTypeEmpty {
+					return true
+				}
+			}
+		}
+		return false
+	}
 	return true
 }
 
@@ -50,15 +98,15 @@ func NewMove(from int, to int) Move {
 }
 
 func NewMoveByXY(fromX int, fromY int, toX int, toY int) Move {
-	return NewMove(XYToIndex(fromX, fromY), XYToIndex(toX, toY))
+	return NewMove(xyToIndex(fromX, fromY), xyToIndex(toX, toY))
 }
 
-func IndexToXY(index int) (x int, y int) {
+func indexToXY(index int) (x int, y int) {
 	x = index % 5
 	y = index / 5
 	return x, y
 }
 
-func XYToIndex(x int, y int) int {
+func xyToIndex(x int, y int) int {
 	return x + y*5
 }
